78ceae377e69cd8dec235ec561c49583e2575391
Jose <jomoespe@gmail.com>
2017-03-29 17:23:42 +0200

    Add Zuul + ConvergentUI + SpringBoot support

---
+++ src/main/java/org/thetangram/triangle/Application.java
@@ -0,0 +1,23 @@
+package org.thetangram.triangle;
+
+import static org.springframework.boot.SpringApplication.run;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+@ComponentScan(basePackages = {"net.acesinc"})
+public class Application {
+    public static void main(String[] args) {
+		run(Application.class, args);
+	}
+    
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }    
+}
