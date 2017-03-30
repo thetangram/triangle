c191287b246e2e99e74e665a2cdc18906cac7f85
Jose Moreno <jose.morenoesteban@zooplus.com>
2017-03-30 16:58:00 +0200

    Add enable autoconfiguration support

--- src/main/java/org/thetangram/triangle/TriangleApplication.java
+++ src/main/java/org/thetangram/triangle/TriangleApplication.java
@@ -2,6 +2,7 @@ package org.thetangram.triangle;
 
 import static org.springframework.boot.SpringApplication.run;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableAutoConfiguration
 @ComponentScan(basePackages = {"net.acesinc"})
 public class TriangleApplication {
     public static void main(String...args) {
